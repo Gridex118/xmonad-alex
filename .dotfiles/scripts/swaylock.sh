6d29980bd462fd9e2b6db1872a37fe2dd568e763
Alex Rosegrid <rosegrid58@gmail.com>
2026-06-17 21:15:48 +0530

    chore: swaylock will randomly pick a wallpaper to use

--- .dotfiles/scripts/swaylock.sh
+++ .dotfiles/scripts/swaylock.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BG_IMAGE="$HOME/Wallpapers/lockscreen.png"
+BG_IMAGE_DIR="$HOME/Wallpapers/PGR_rotate"
+BG_IMAGE="${BG_IMAGE_DIR}/$(ls $BG_IMAGE_DIR| shuf -n1)"
 
 if pgrep -x swaylock &>/dev/null; then
     killall swaylock
